ce401ccfb755f24be33c5a6d69867b068ec4407d
Jose Mendez <75923907+jochea@users.noreply.github.com>
2025-05-07 13:06:53 -0400

    Add files via upload

---
+++ vivero_vital/main.py
@@ -0,0 +1,8 @@
+
+import tkinter as tk
+from controlador.controlador import ControladorApp
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = ControladorApp(root)
+    root.mainloop()
---
+++ vivero_vital/modelo/invernadero_modelo.py
@@ -0,0 +1,9 @@
+
+class Invernadero:
+    def __init__(self, nombre, superficie, tipo_cultivo, sistema_riego, capacidad, responsable):
+        self.nombre = nombre
+        self.superficie = superficie
+        self.tipo_cultivo = tipo_cultivo
+        self.sistema_riego = sistema_riego
+        self.capacidad = capacidad
+        self.responsable = responsable
