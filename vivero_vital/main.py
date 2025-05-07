5265914353d674ab85c9a134eab5663a61ce0f96
Jose Mendez <75923907+jochea@users.noreply.github.com>
2025-05-07 15:16:26 -0400

    Delete vivero_vital directory

--- vivero_vital/main.py
+++
@@ -1,8 +0,0 @@
-
-import tkinter as tk
-from controlador.controlador import ControladorApp
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    app = ControladorApp(root)
-    root.mainloop()
--- vivero_vital/modelo/invernadero_modelo.py
+++
@@ -1,9 +0,0 @@
-
-class Invernadero:
-    def __init__(self, nombre, superficie, tipo_cultivo, sistema_riego, capacidad, responsable):
-        self.nombre = nombre
-        self.superficie = superficie
-        self.tipo_cultivo = tipo_cultivo
-        self.sistema_riego = sistema_riego
-        self.capacidad = capacidad
-        self.responsable = responsable
