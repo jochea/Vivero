0f07f123f75fa4f8202176de8076b89af1746fe9
Jose Mendez <75923907+jochea@users.noreply.github.com>
2025-05-07 15:30:47 -0500

    modelo.py

--- vivero_vital_flat/modelo.py
+++ vivero_vital_flat/modelo.py
@@ -8,14 +8,6 @@ class Invernadero:
         self.capacidad = capacidad
         self.responsable = responsable
 
-class Invernadero:
-    def __init__(self, nombre, superficie, tipo_cultivo, sistema_riego, capacidad, responsable):
-        self.nombre = nombre
-        self.superficie = superficie
-        self.tipo_cultivo = tipo_cultivo
-        self.sistema_riego = sistema_riego
-        self.capacidad = capacidad
-        self.responsable = responsable
 # Metodos get 
 
 def get_nombre (self):
